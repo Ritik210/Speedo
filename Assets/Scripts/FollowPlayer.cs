f89c85e558cc30e6ed47ed1c233027e037c91153
Ritik210 <hritikagarwal50@gmail.com>
2020-09-07 21:59:31 +0530

    fp

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    
+
+    public Vector3 offset;
+    [Range(0.01f,1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    public bool lookAtPlayer = false;
+    private void Start()
+    {
+        offset = transform.position - player.position; 
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = player.position + offset;
+        //Vector3 sPos = Vector3.Lerp(transform.position, dPos, sSpeed * Time.deltaTime);
+        //  Debug.Log(player.position);
+        transform.position = Vector3.Slerp(transform.position,newPos,SmoothFactor);
+
+        if (lookAtPlayer)
+        transform.LookAt(player);
+    }
+}
